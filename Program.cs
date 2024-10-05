3d402d422e1071f8521ff50cc60c1ebab6116528
jgrissom <jgrissom91@gmail.com>
2024-10-05 09:37:15 -0500

    [1.03] Create Json Object

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string JsonString = """
+  {
+    "Name": "Baseball"
+  }
+""";
+Console.WriteLine(JsonString);
