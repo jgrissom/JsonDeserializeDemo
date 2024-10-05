340d1595ac725825fb307767e02dac1b784716f0
jgrissom <jgrissom91@gmail.com>
2024-10-05 10:21:38 -0500

    [1.09] Deserialize Json array of objects (from file) into dotnet list of objects

--- Program.cs
+++ Program.cs
@@ -1,27 +1,6 @@
 ﻿using System.Text.Json;
 
-string JsonString = """
-  [
-    {
-      "Name": "⚾ Baseball",
-      "Posts": [
-        { "Title": "Pitchers", "Description": "Tobias Myers is my favorite pitcher" },
-        { "Title": "Outfielders", "Description": "Jackson Chourio is my favorite outfielder" }
-      ]
-    },
-    {
-      "Name": "😳 Exercise",
-      "Posts": [
-        { "Title": "Yoga", "Description": "Yoga is a wonderful exercise - Namaste" },
-        { "Title": "Cycling", "Description": "is FUN" },
-        { "Title": "Weight Training", "Description": "Builds more muscle" }
-      ]
-    }
-  ]
-""";
-// Console.WriteLine(JsonString);
-
-List<Blog> blogs = JsonSerializer.Deserialize<List<Blog>>(JsonString)!;
+List<Blog> blogs = JsonSerializer.Deserialize<List<Blog>>(File.ReadAllText("blogs.json"))!;
 foreach (Blog blog in blogs)
 {
   Console.WriteLine(blog.Name);
